61413d3348e0e93133ae8a3b30b6128602adcf55
jammoody <jammoody@gmail.com>
2025-08-19 12:54:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,3 @@
 // pages/_app.js
 import '../styles/globals.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
